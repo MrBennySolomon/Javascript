f0045d2c1c47b3d2343f3ddb0ac254e3fa0c2738
Benny-Matan <bensol@h-MacBook-Air-sl-bensol-4.local>
2023-01-08 10:55:28 +0200

    1.1_primitive_data_types ver 1.1

---
+++ 1.1_primitive_data_types/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let str = new string("Hello World");
+let num = 123;
+let obj = {
+  name: "Benny",
+  lastName: "Solomon"
+};
+let booleanValue = true;
+let nullValue = null;
+let undefinedValue = undefined;
+let symbolValue = 'unique';
