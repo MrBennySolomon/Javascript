37b5ddca90ee5037a12fc366c3bb1167f43fc5be
Benny-Matan <bensol@h-MacBook-Air-sl-bensol-4.local>
2023-01-08 13:25:43 +0200

    2.2_functions

--- 2.2_functions/script.js
+++ 2.2_functions/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+function tellFortune (jobTitle, location, partnerName, numberOfChildren) {
+  return `
+  You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren} children
+  `;
+}
