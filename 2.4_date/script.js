2ec244be239be4cef632c400218fd2764a35faca
Benny-Matan <bensol@h-MacBook-Air-sl-bensol-4.local>
2023-01-08 15:00:23 +0200

    2.4

--- 2.4_date/script.js
+++ 2.4_date/script.js
@@ -1,33 +1,13 @@
 'use script';
 
-let month=new Array(12);
-month[0]="January";
-month[1]="Ferbuary";
-month[2]="Merch";
-month[3]="April";
-month[4]="May";
-month[5]="June";
-month[6]="July";
-month[6]="August";
-month[6]="September";
-month[6]="October";
-month[6]="November";
-month[6]="December";
-
-let weekday=new Array(7);
-weekday[0]="Sunday";
-weekday[1]="Monday";
-weekday[2]="Tuesday";
-weekday[3]="Wednesday";
-weekday[4]="Thursday";
-weekday[5]="Friday";
-weekday[6]="Saturday";
-
 function getDate () {
   const date = new Date();
-  return `
-  Today is ${weekday[date.getDay()]} the ${date.getDate()} of ${month[date.getMonth()]} ${date.getFullYear()}
-  `;
+  let day = date.toLocaleString("en-us", { weekday: "long" });
+  let month = date.toLocaleString("en-us", { month: "long" });
+  let dateNum = date.getDate();
+  let year = date.getFullYear();
+
+  return `Today is ${day} the ${dateNum} of ${month} ${year}`;
 }
 
 console.log (getDate());
