2af8f4c3f8694fbdf60d8e8d31dc786944224a94
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-09 11:44:59 +0200

    4.2_grade_assigner

--- 4.2_grade_assigner/script.js
+++ 4.2_grade_assigner/script.js
@@ -1,2 +1,20 @@
-'use strict'
 
+function gradeAssigner (score) {
+
+  let grade;
+  if (score >= 0 && score <= 64) {
+    grade = 'F'
+  }else if (score >= 65 && score <= 69) {
+    grade = 'D';
+  }else if (score >= 70 && score <= 79) {
+    grade = 'C';
+  }else if (score >= 80 && score <= 89) {
+    grade = 'B';
+  }else if (score >= 90 && score <= 100) {
+    grade = 'A';
+  }
+
+  return grade
+}
+
+console.log(gradeAssigner(89));
