725e689db46ac4156098f9da86a99c9596d32d5c
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-09 11:37:13 +0200

    4.1

---  4.1_number_of_siblings/script.js
+++  4.1_number_of_siblings/script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+const numSiblings = prompt ('How many siblings do you have?');
+
+if (numSiblings === '1') {
+  console.log('1 sibling');
+}else if (numSiblings > '1') {
+  console.log('More than 1 sibling');
+}else {
+  console.log('No siblings');
+
+}
