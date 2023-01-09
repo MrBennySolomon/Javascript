d0ed7b03c1ae67d106c4329bebf9620a33ca92b7
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-09 13:57:06 +0200

    4.5_leap_year

---
+++ 4.5_leap_year/script.js
@@ -0,0 +1,10 @@
+function checkLeapYear(year) {
+
+  if (((year % 4) == 0) && ((year % 100) != 0) || ((year % 400) == 0)) {
+      console.log(year + ' is a leap year');
+  } else {
+      console.log(year + ' is not a leap year');
+  }
+}
+
+checkLeapYear(2012);
