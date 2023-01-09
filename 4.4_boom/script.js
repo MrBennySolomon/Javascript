0241f4447d8e45b28086c72675d1fcdfc29420df
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-09 13:44:06 +0200

    4.4_boom

--- 4.4_boom/script.js
+++ 4.4_boom/script.js
@@ -1,3 +1,15 @@
-function boom (arrNum) {
-  
+function boom (n, boomNum) {
+  for (let i = 1; i <= n; i++) {
+    if ((i === boomNum) || ((i % boomNum) == 0)) {
+      if ((i + '').indexOf(boomNum) > -1) {
+        console.log('BOOM-BOOM');
+      }else {
+        console.log('BOOM');
+      }
+    }else {
+      console.log(i);
+    }
+  }
 }
+
+boom(15, 7);
