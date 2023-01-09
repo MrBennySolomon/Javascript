49dd9e1ddd65a61aec88f5ad3379d59766ffaed5
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-09 12:57:20 +0200

    4.3_basketball_score

---
+++ 4.3_basketball_score/script.js
@@ -0,0 +1,37 @@
+
+function average (arr) {
+
+  let sum = 0;
+
+  for (let i of arr) {
+    sum += i;
+  }
+  return (sum / arr.length); 
+}
+
+function winner (arrTeamAndScore) {
+  let highestScore = [0, 'a', 'tie'] ;
+
+  for (let i of arrTeamAndScore) {
+
+    if (highestScore[0] < Math.max(i[1])) {
+      highestScore[0] = Math.max(i[1]);
+      highestScore[1] = i[0];
+      highestScore[2] = 'winner';
+    }else if (highestScore[0] === Math.max(i[1])) {
+        highestScore[0] = Math.max(i[1]);
+        highestScore[1] = i[0];
+        highestScore[2] = 'tie';
+    }
+  }
+  return highestScore;
+}
+
+
+//console.log(winner([johnTeam, mikeTeam]));
+//console.log(winner([mikeTeam, johnTeam]));
+let johnTeam = average([89, 120, 103]);
+let mikeTeam = average([116, 94, 123]);
+let maryTeam = average([97, 134, 105]);
+
+console.log(winner([['johnTeam',johnTeam], ['mikeTeam',mikeTeam], ['maryTeam',maryTeam]]));
