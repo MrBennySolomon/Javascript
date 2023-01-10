f0d192c0f2bd75ccbc4dc4f0385b4b3956dc9f78
MRBENNYSOLOMON <mr.benny.solomon@gmail.com>
2023-01-10 15:14:36 +0200

    7.2_last_occurance

---
+++ 7.2_last_occurance/script.js
@@ -0,0 +1,6 @@
+const string = "The more you know, the more you know that you don't know";
+const word = 'you';
+
+const lastIndex = (str, word) => str.lastIndexOf(word);
+
+console.log(lastIndex(string, word));
