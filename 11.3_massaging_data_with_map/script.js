17b28a3834fc1ea326a06f972316d40b1c88842a
MrBennySolomon <MrBennySolomon@gmail.com>
2023-01-16 12:46:50 +0200

    11.3_massaging_data_with_map

---
+++ 11.3_massaging_data_with_map/script.js
@@ -0,0 +1,64 @@
+const data = [
+  {
+  name: "John",
+  birthday: "1-1-1995",
+  favoriteFoods: {
+  meats: ["hamburgers", "sausages"],
+  fish: ["salmon", "pike"],
+  },
+  },
+  {
+  name: "Mark",
+  birthday: "10-5-1980",
+  favoriteFoods: {
+  meats: ["hamburgers", "steak", "lamb"],
+  fish: ["tuna", "salmon", "barracuda"],
+  },
+  },
+  {
+  name: "Mary",
+  birthday: "1-10-1977",
+  favoriteFoods: {
+  meats: ["ham", "chicken"],
+  fish: ["pike"],
+  },
+  },
+  {
+  name: "Thomas",
+  birthday: "1-10-1990",
+  favoriteFoods: {
+  meats: ["bird", "rooster"],
+  fish: ["salmon"],
+  },
+  },
+  {
+  name: "Mary",
+  birthday: "1-10-1977",
+  favoriteFoods: {
+  meats: ["hamburgers", "lamb"],
+  fish: ["anchovies", "tuna"],
+  },
+  },
+  ];
+
+// 1. Create a function that returns all the names from the array.
+const names = data => {
+  return data.map(item => item.name);
+}
+console.log(names(data));
+// 2. Create a function that returns all the objects that are born
+// before 1990.
+const before1990 = data => {
+  let newObj = [];
+  data.forEach(element => {
+    const year = +(element.birthday.split("-")[2]);
+    if (year < 1990) {
+      newObj.push(element);
+  }
+  });
+  return newObj;
+}
+console.log(before1990(data));
+// 3. Create a function that returns an object of all the different
+// foods from all the objects as the key and the number of
+// times that food is present in all the objects as the value.
