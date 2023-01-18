192e7607f20a096ff01756d2b7dfebb91ec45986
bensol <mr.benny.solomon@gmail.com>
2023-01-18 14:02:39 +0200

    17.1_closures

---
+++ 17.1_closures/script.js
@@ -0,0 +1,34 @@
+//Block 1
+var b = 1;
+function someFunction(number) {
+  function otherFunction(input) {
+    return b;
+  }
+  b = 5;
+  return otherFunction;
+}
+var firstResult = someFunction(9);
+var result = firstResult(2);//5
+
+
+
+//Block 2
+var a = 1;
+function b2() {
+  a = 10;
+  return;
+  function a() { }
+}
+b2();
+console.log(a);//1
+
+
+
+//Block 3
+let i;
+for (i = 0; i < 3; i++) {
+  const log = () => {
+    console.log(i);
+  }
+  setTimeout(log, 100);//0 1 2
+}
